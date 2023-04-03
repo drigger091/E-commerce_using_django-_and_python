9da0410966a478ee84a503348e9ca3783a435615
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-04-04 00:18:17 +0530

    updating category search options

--- store/models.py
+++ store/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
 
+    def get_absolute_url(self):
+        return reverse('store:product_detail',args=[self.slug])
+
 
     def __str__(self):
         return self.name
